57c266a17ea7f04767d750f416a1b7ea9e655854
M.Mezni <mamezni@gmail.com>
2026-08-08 20:29:38 -0400

    Cleanup

--- consumers/rdv-web/src/app/models.py
+++
@@ -1,9 +0,0 @@
-import uuid
-from datetime import datetime, time
-
-from sqlalchemy import TIMESTAMP, Column, ForeignKey, String, Integer, Float, Boolean, text, Date, Time, Sequence, Identity
-from sqlalchemy.orm import relationship
-from sqlalchemy.dialects.postgresql import UUID, ARRAY
-
-from sqlalchemy.orm import declarative_base
-Base = declarative_base()
